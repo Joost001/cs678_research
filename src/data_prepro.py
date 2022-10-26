33d111416aab9b8b9ad483c1c0c9522494766a88
Jonathan Vasquez Verdugo <jovasque156@gmail.com>
2022-10-25 20:13:37 -0400

    Add CoVoST dataclass and process function. Also, add the set of functions in utils.py' :

--- src/data_prepro.py
+++ src/data_prepro.py
@@ -1,21 +1,23 @@
 ## Based on the fairseq repo: https://github.com/facebookresearch/fairseq
+#Torch
+import torch
+import torchaudio
+import torchaudio.functional as F
+import torchaudio.transforms as T
+from torchaudio.datasets.utils import download_url, extract_archive
+from torch.utils.data import Dataset, DataLoader
+from torch import Tensor
 
 #General
 import pandas as pd
 from typing import Tuple, Optional
 from pathlib import Path
-from utils import load_df_from_tsv
-from torchaudio.datasets.utils import download_url, extract_archive
-
-#Torch
-import torchaudio
-import torchaudio.functional as F
-import torchaudio.transforms as T
-from torch.utils.data import Dataset, DataLoader
-from torch import Tensor
-
-#Processing
-import librosa
+from utils import load_df_from_tsv, extract_fbank_features, filter_manifest_df, save_df_to_tsv, create_zip, get_zip_manifest
+import argparse
+from tqdm import tqdm
+import os
+import io
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 COLUMNS = ['id', 'audio', 'n_frames', 'tgt_text', 'speaker']
 
@@ -51,7 +53,7 @@ class CoVoST(Dataset):
         
         # Retrieve path where are the files, then assert if it contains the files
         self.root = Path(root)
-        cv_tsv_path = self.root/'validated.tsv'
+        cv_tsv_path = self.root / 'validated.tsv'
         assert cv_tsv_path.is_file()
 
         # Download translations in case they are not in the path
@@ -67,8 +69,9 @@ class CoVoST(Dataset):
         # Merge the tsv files from the source lang with translation from the target lang
         cv_tsv = load_df_from_tsv(cv_tsv_path)
         covost_tsv = load_df_from_tsv(self.root / Path(covost_url).name.replace('.tar.gz', ''))
+
         df = pd.merge(
-            left = cv_tsv[['path', 'sentence', 'cliente_id']],
+            left = cv_tsv[['path', 'sentence', 'client_id']],
             right = covost_tsv[['path', 'translation', 'split']],
             how = 'inner',
             on = 'path'
@@ -79,7 +82,7 @@ class CoVoST(Dataset):
         # label 'train_covost'. In the paper these examples are also included in the training 
         assert split in self.SPLITS
         if split=='train':
-            df = df[(df['split']) == split | (df['split'] == f'{split}_covost')]
+            df = df[(df['split'] == split) | (df['split'] == f'{split}_covost')]
         else:
             df = df[df['split'] == split]
 
@@ -93,6 +96,9 @@ class CoVoST(Dataset):
                 self.data.append(e)
             except RuntimeError:
                 pass
+    
+    def __len__(self):
+        return len(self.data)
 
     def __getitem__(self, n: int) -> Tuple[Tensor, int, str, str, Optional[str], str, str]:
         '''
@@ -107,6 +113,7 @@ class CoVoST(Dataset):
         data = self.data[n]
         path = self.root / 'clips' / data['path']
         waveform, sample_rate = torchaudio.load(path)
+        waveform = torch.tensor(waveform)
         sentence = data['sentence']
         translation = data['translation']
         speaker_id = data['cliente_id']
@@ -119,3 +126,66 @@ class CoVoST(Dataset):
 # loader = iter(DataLoader(CoVoST(root, split, source_language, target_language),
 #                           batch_size=bs, 
 #                           shuffle=True))
+
+
+def process(args):
+    '''
+    Process the dataset
+    '''
+    print('processing')
+    # Retreive the root folder that contains the audio from source langauge
+    root = Path(args.data_root).absolute() / args.src_lang
+    if not root.is_dir():
+        raise NotADirectoryError(f"{root} does not exist")
+    
+    # Extract features by using the required preprocessing procedures
+    print('starting features extraction')
+    feature_root = root / "fbank80"
+    feature_root.mkdir(exist_ok=True)
+    for split in CoVoST.SPLITS:
+        print(f"Fetching split {split}...")
+        dataset = CoVoST(root, split, args.src_lang, args.tgt_lang)
+        print("Extracting log mel filter bank features...")
+        for waveform, sample_rate, _, _, _, utt_id in tqdm(dataset):
+            extract_fbank_features(
+                waveform, sample_rate, feature_root / f"{utt_id}.npy"
+            )
+
+    # Pack features into ZIP
+    zip_path = root / "fbank80.zip"
+    print("ZIPing features...")
+    create_zip(feature_root, zip_path)
+    print("Fetching ZIP manifest...")
+    audio_paths, audio_lengths = get_zip_manifest(zip_path)
+
+    # Generate TSV manifest
+    print("Generating manifest...")
+    train_text = []
+    task = f"st_{args.src_lang}_{args.tgt_lang}"
+    for split in CoVoST.SPLITS:
+        manifest = {c: [] for c in COLUMNS}
+        dataset = CoVoST(root, split, args.src_lang, args.tgt_lang)
+        for _, _, src_utt, tgt_utt, speaker_id, utt_id in tqdm(dataset):
+            manifest["id"].append(utt_id)
+            manifest["audio"].append(audio_paths[utt_id])
+            manifest["n_frames"].append(audio_lengths[utt_id])
+            manifest["tgt_text"].append(src_utt if args.tgt_lang is None else tgt_utt)
+            manifest["speaker"].append(speaker_id)
+        is_train_split = split.startswith("train")
+        if is_train_split:
+            train_text.extend(manifest["tgt_text"])
+        df = pd.DataFrame.from_dict(manifest)
+        df = filter_manifest_df(df, is_train_split=is_train_split)
+        save_df_to_tsv(df, root / f"{split}_{task}.tsv")
+
+if __name__ == '__main__':
+    print('Reading arguments...')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data-root", "-d", required=True, type=str, help="data root with sub-folders for each language <root>/<src_lang>")
+    parser.add_argument("--src-lang", "-s", required=True, type=str)
+    parser.add_argument("--tgt-lang", "-t", type=str)
+    
+    args = parser.parse_args()
+
+    print('Starting preprocessing...')
+    process(args)
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,174 @@
+## Based on the fairseq repo: https://github.com/facebookresearch/fairseq
+
+# Torch, Audio
+import torch
+import torchaudio.sox_effects as ta_sox
+from torchaudio.functional import melscale_fbanks
+import soundfile as sf
+import torchaudio.compliance.kaldi as ta_kaldi
+
+# General
+import io
+import csv
+import zipfile
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
 from pathlib import Path
-from typing import Union
+from functools import reduce
+from typing import Union, Optional, Tuple
+
 
 def load_df_from_tsv(path: Union[str, Path]) -> pd.DataFrame: #note that Union[str, Path] is equal to str | Path
     _path = path if isinstance(path, str) else path.as_posix() #as_posix() Return a string representation of the path with forward slashes (/). Ex: p.as_posix()->'c:/windows'
-    return pd.read_csv(_path, sep='\t', header=0, encoding='utf-8', escapeachar='\\', quoting=csv.QUOTE_NONE, na_filter=False)
+    return pd.read_csv(_path, sep='\t', header=0, encoding='utf-8', escapechar='\\', quoting=csv.QUOTE_NONE, na_filter=False)
+
+def get_zip_manifest(zip_path: Path, zip_root: Optional[Path] = None, is_audio=False):
+    _zip_path = Path.joinpath(zip_root or Path(""), zip_path)
+    with zipfile.ZipFile(_zip_path, mode="r") as f:
+        info = f.infolist()
+    paths, lengths = {}, {}
+    for i in tqdm(info):
+        utt_id = Path(i.filename).stem
+        offset, file_size = i.header_offset + 30 + len(i.filename), i.file_size
+        paths[utt_id] = f"{zip_path.as_posix()}:{offset}:{file_size}"
+        with open(_zip_path, "rb") as f:
+            f.seek(offset)
+            byte_data = f.read(file_size)
+            assert len(byte_data) > 1
+            byte_data_fp = io.BytesIO(byte_data)
+            if is_audio:
+                lengths[utt_id] = sf.info(byte_data_fp).frames
+            else:
+                lengths[utt_id] = np.load(byte_data_fp).shape[0]
+    return paths, lengths
+
+def extract_fbank_features(waveform: torch.FloatTensor, sample_rate: int, output_path: Optional[Path] = None,
+                            n_mel_bins: int = 80, overwrite: bool = False):
+    
+    if output_path is not None and output_path.is_file() and not overwrite:
+        return
+
+    _waveform, _ = convert_waveform(waveform, sample_rate, to_mono=True)
+    # Kaldi compliance: 16-bit signed integers
+    _waveform = _waveform * (2 ** 15)
+    _waveform = _waveform.numpy()
+
+    features = _get_torchaudio_fbank(_waveform, sample_rate, n_mel_bins)
+    if features is None:
+        raise ImportError(
+            "Please install pyKaldi or torchaudio to enable fbank feature extraction"
+        )
+
+    if output_path is not None:
+        np.save(output_path.as_posix(), features)
+    
+    return features
+
+def convert_waveform(waveform: Union[np.ndarray, torch.Tensor], sample_rate: int, normalize_volume: bool = False,
+                    to_mono: bool = False, to_sample_rate: Optional[int] = None) -> Tuple[Union[np.ndarray, torch.Tensor], int]:
+    """convert a waveform:
+    - to a target sample rate
+    - from multi-channel to mono channel
+    - volume normalization
+
+    Args:
+        waveform (numpy.ndarray or torch.Tensor): 2D original waveform
+            (channels x length)
+        sample_rate (int): original sample rate
+        normalize_volume (bool): perform volume normalization
+        to_mono (bool): convert to mono channel if having multiple channels
+        to_sample_rate (Optional[int]): target sample rate
+    Returns:
+        waveform (numpy.ndarray): converted 2D waveform (channels x length)
+        sample_rate (float): target sample rate
+    """
+
+    effects = []
+    if normalize_volume:
+        effects.append(["gain", "-n"])
+    if to_sample_rate is not None and to_sample_rate != sample_rate:
+        effects.append(["rate", f"{to_sample_rate}"])
+    if to_mono and waveform.shape[0] > 1:
+        effects.append(["channels", "1"])
+    if len(effects) > 0:
+        is_np_input = isinstance(waveform, np.ndarray)
+        _waveform = torch.from_numpy(waveform) if is_np_input else waveform
+        converted, converted_sample_rate = ta_sox.apply_effects_tensor(
+            _waveform, sample_rate, effects
+        )
+        if is_np_input:
+            converted = converted.numpy()
+        return converted, converted_sample_rate
+    return waveform, sample_rate
+
+def _get_torchaudio_fbank(waveform: np.ndarray, sample_rate, n_bins=80) -> Optional[np.ndarray]:
+    """Get mel-filter bank features via TorchAudio."""
+    import torchaudio.compliance.kaldi as ta_kaldi
+
+    waveform = torch.from_numpy(waveform)
+    features = ta_kaldi.fbank(
+        waveform, num_mel_bins=n_bins, sample_frequency=sample_rate
+    )
+    return features.numpy()
+
+def filter_manifest_df(
+    df, is_train_split=False, extra_filters=None, min_n_frames=5, max_n_frames=3000
+):
+    filters = {"no speech": df["audio"] == "",
+                f"short speech (<{min_n_frames} frames)": df["n_frames"] < min_n_frames,
+                "empty sentence": df["tgt_text"] == ""
+    }
+    
+    if is_train_split:
+        filters[f"long speech (>{max_n_frames} frames)"] = df["n_frames"] > max_n_frames
+    if extra_filters is not None:
+        filters.update(extra_filters)
+    
+    invalid = reduce(lambda x, y: x | y, filters.values())
+    valid = ~invalid
+    print(
+        "| "
+        + ", ".join(f"{n}: {f.sum()}" for n, f in filters.items())
+        + f", total {invalid.sum()} filtered, {valid.sum()} remained."
+    )
+    return df[valid]
+
+def save_df_to_tsv(dataframe, path: Union[str, Path]):
+    _path = path if isinstance(path, str) else path.as_posix()
+    dataframe.to_csv(
+        _path,
+        sep="\t",
+        header=True,
+        index=False,
+        encoding="utf-8",
+        escapechar="\\",
+        quoting=csv.QUOTE_NONE,
+    )
+
+def create_zip(data_root: Path, zip_path: Path):
+    paths = list(data_root.glob("*.npy"))
+    with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_STORED) as f:
+        for path in tqdm(paths):
+            f.write(path, arcname=path.name)
+
+def get_zip_manifest(zip_path: Path, zip_root: Optional[Path] = None):
+    _zip_path = Path.joinpath(zip_root or Path(""), zip_path)
+    with zipfile.ZipFile(_zip_path, mode="r") as f:
+        info = f.infolist()
+    paths, lengths = {}, {}
+    for i in tqdm(info):
+        utt_id = Path(i.filename).stem
+        offset, file_size = i.header_offset + 30 + len(i.filename), i.file_size
+        paths[utt_id] = f"{zip_path.as_posix()}:{offset}:{file_size}"
+        with open(_zip_path, "rb") as f:
+            f.seek(offset)
+            byte_data = f.read(file_size)
+            assert len(byte_data) > 1
+            assert is_npy_data(byte_data), i
+            byte_data_fp = io.BytesIO(byte_data)
+            lengths[utt_id] = np.load(byte_data_fp).shape[0]
+    return paths, lengths
+
+def is_npy_data(data: bytes) -> bool:
+    return data[0] == 147 and data[1] == 78
